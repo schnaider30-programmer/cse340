433f0d12da7fcb3d4ded771f85827607c4e2b706
schnaider30-programmer <sjeanlouis@byupathway.edu>
2026-01-19 16:50:03 -0500

    format SQL code

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,27 +1,39 @@
 -- QUERY 1
-INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password) 
-VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
-
+INSERT INTO public.account (
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    );
 -- QUERY 2
-UPDATE public.account 
+UPDATE public.account
 SET account_type = 'Admin'
 WHERE account_id = 1;
-
 -- QUERY 3
-DELETE FROM public.account 
+DELETE FROM public.account
 WHERE account_id = 1;
-
 -- QUERY 4
-UPDATE public.inventory 
-SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+UPDATE public.inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'small interiors',
+        'a huge interior'
+    )
 WHERE inv_id = 10;
-
 -- QYERY 5
-SELECT inv_make, inv_model, classification_name 
-FROM public.inventory AS i JOIN public.classification AS c 
-ON i.classification_id = c.classification_id
+SELECT inv_make,
+    inv_model,
+    classification_name
+FROM public.inventory AS i
+    JOIN public.classification AS c ON i.classification_id = c.classification_id
 WHERE c.classification_id = 2;
-
 -- QUERY 6
-UPDATE public.inventory 
-SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'), inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
